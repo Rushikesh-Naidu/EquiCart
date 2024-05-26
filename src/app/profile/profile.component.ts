9063ccfa6bae73e081394af3fb73705d45f6be6e
Rushikesh-Naidu <rushikeshnaidu.t@gmail.com>
2024-05-26 13:18:23 +0530

    My Profile completed

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -13,10 +17,30 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     CommonModule,
     MatIconModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
 
+  profileForm: FormGroup
+  submitted: any;
+  needToEdit: boolean = false;
+
+  constructor(
+    private formBuilder : FormBuilder,
+  ){}
+
+  ngOnInit(){
+    this.profileForm = this.formBuilder.group({
+      name: ['Rushikesh'],
+      phone: ['9999999999'],
+      email: ['rushikeshnaidu.t@gmail.com'],
+    })
+  }
 }
